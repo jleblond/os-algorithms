9d15000c1f982f370bac84f655f3ffa65d8d3e1a
jleblond <jasmine.leblondc@gmail.com>
2020-11-18 00:33:16 -0500

    Add Amdahl's Law

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from disk_scheduling import algorithms
+from threads import laws
+
+
+laws.amdahls_law(0.25, 2)
 
 start_at = 5
 arr = [1, 3, 7, 4, 10, 12, 8]
 is_moving_towards_0 = True  # moving towards 0
+lower_cylinder = 0
+upper_cylinder = 14
 
 algorithms.fcfs(start_at, arr)
 algorithms.scan(start_at, arr, is_moving_towards_0)
@@ -11,6 +17,8 @@ algorithms.scan(start_at, arr, is_moving_towards_0)
 start_at_2 = 53
 arr_2 = [98, 183, 32, 122, 14, 124, 65, 67]
 is_moving_towards_0 = False  # moving towards end
+lower_cylinder = 0
+upper_cylinder = 199
 
 algorithms.fcfs(start_at_2, arr_2)
 algorithms.scan(start_at_2, arr_2, is_moving_towards_0)
---
+++ threads/laws.py
@@ -0,0 +1,15 @@
+
+def amdahls_law(s, n):
+    """
+    Amdahl's Law
+    Identifies performance gains from adding additional cores to an application that has both serial and parallel components.
+    Serial portion of an application has disproportionate effect on performance gained by adding additional cores
+    :param s: S is serial portion (% of serial portions in the app, value format ranging from 0.0 to 1.0)
+    :param n: N processing cores
+    :return: speed_up
+    """
+    speed_up = 1/(s+((1-s)/n))
+    print("speedup <= " + str(speed_up))
+    print("That is, if application is " + str((1-s)*100) + "% parallel / " + str(s*100) + "% serial, "
+           "moving from 1 to " + str(n) + " cores results in speedup of " + str(speed_up) + ' times')
+    return speed_up
