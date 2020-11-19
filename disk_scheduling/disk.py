281107a533ff473cf5c261a9736975a82569765f
jleblond <jasmine.leblondc@gmail.com>
2020-11-18 23:25:15 -0500

    Change class function name for more clarity

--- disk_scheduling/disk.py
+++ disk_scheduling/disk.py
@@ -10,20 +10,25 @@ class Disk:
 
         self.is_moving_towards_0 = is_moving_towards_0
 
-    def request_queue_copy(self):
+    def requests_queue_copy(self):
         return self.requests_queue.copy()
 
     def compute(self):
-        algorithms.fcfs(self.request_queue_copy(), self.head_cylinder)
+        algorithms.fcfs(self.requests_queue_copy(), self.head_cylinder)
 
-        algorithms.sstf(self.request_queue_copy(), self.head_cylinder)
+        algorithms.sstf(self.requests_queue_copy(), self.head_cylinder)
 
-        algorithms.scan(self.request_queue_copy(),
+        algorithms.scan(self.requests_queue_copy(),
                         self.head_cylinder,
                         self.lower_cylinder, self.upper_cylinder,
                         self.is_moving_towards_0)
 
-        algorithms.c_look(self.request_queue_copy(), self.head_cylinder, self.is_moving_towards_0)
+        algorithms.c_scan(self.requests_queue_copy(),
+                        self.head_cylinder,
+                        self.lower_cylinder, self.upper_cylinder,
+                        self.is_moving_towards_0)
+
+        algorithms.c_look(self.requests_queue_copy(), self.head_cylinder, self.is_moving_towards_0)
 
 
 
