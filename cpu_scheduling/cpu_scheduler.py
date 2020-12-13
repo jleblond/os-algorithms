1f04c6854df49920c068c242d0f099b8404bb832
jleblond <jasmine.leblondc@gmail.com>
2020-12-13 00:57:17 -0500

    Error messages if args missing for CPU scheduling computing

--- cpu_scheduling/cpu_scheduler.py
+++ cpu_scheduling/cpu_scheduler.py
@@ -5,19 +5,40 @@ class CpuScheduler:
         self.original_processes_list = processes_list
         self.processes_list = processes_list.copy()
         self.quantum = quantum
+        self.errors_message_str = ''
 
-    # def add_process(self, process_name, burst_time, priority):
-    #     process = {'name': process_name, 'burst_time': burst_time, 'priority': priority}
-    #     self.processes_list.append(process)
+    @staticmethod
+    def element_missing_error_message(el, algorithm_name):
+        msg = '!!!!!!!!!!!!!!!!!!!!!\n'
+        msg += '** ' + str(el) + ' is missing \n'
+        msg += '** ' + algorithm_name + ' algorithm NOT COMPUTED\n'
+        msg += '!!!!!!!!!!!!!!!!!!!!!\n'
+        return msg
 
     def compute(self):
-        algorithms.fcfs(self.processes_list)
+        if list(filter(lambda p: 'burst_time' in p, self.processes_list)) == []:
+            print('*** BURST TIME IS MISSING ***')
+            return
+
+        if list(filter(lambda p: 'arrival_time' in p, self.processes_list) )!= []:
+            algorithms.fcfs(self.processes_list)
+            algorithms.shortest_remaining_time_first(self.processes_list)
+        else:
+            self.errors_message_str += CpuScheduler.element_missing_error_message('arrival_time', 'FCFS & SRTF')
+
+
         algorithms.sjf(self.processes_list)
-        # algorithms.priority(self.processes_list)
-        algorithms.shortest_remaining_time_first(self.processes_list)
+
+        if list(filter(lambda p: 'priority' in p, self.processes_list)) != []:
+            algorithms.priority(self.processes_list)
+        else:
+            self.errors_message_str += CpuScheduler.element_missing_error_message('priority', 'PRIORITY')
+
+
         if self.quantum != None:
             algorithms.round_robin(self.processes_list, self.quantum)
         else:
-            print('* Quantum is NONE - Round-Robin algorithm could not be computed')
+            self.errors_message_str += CpuScheduler.element_missing_error_message('[scheduler quantum is None]', 'ROUND-ROBIN')
 
-
+        print()
+        print(self.errors_message_str)
--- cpu_scheduling/visualizations.py
+++ cpu_scheduling/visualizations.py
@@ -18,7 +18,7 @@ def plot_gantt_chart(algorithm_name, processes_list, processes_scheduled_list):
     y_lim = 15 + 10 * len(processes_list)
     gnt.set_ylim(0, y_lim + 5)
     x_lim = sum(list(map(lambda p: p['burst_time'], processes_list)))
-    gnt.set_xlim(0, x_lim + 5)
+    gnt.set_xlim(0, x_lim + 2)
 
     gnt.set_xlabel('burst cycles')
     gnt.set_ylabel('process')
@@ -34,7 +34,9 @@ def plot_gantt_chart(algorithm_name, processes_list, processes_scheduled_list):
     gnt.set_yticks(y_ticks)
     gnt.set_yticklabels(y_labels)
 
-    loc = plticker.MultipleLocator(base=5)  # this locator puts ticks at regular intervals
+    gnt.tick_params(axis="x", labelsize=6)
+
+    loc = plticker.MultipleLocator(base=2)  # this locator puts ticks at regular intervals
     gnt.xaxis.set_major_locator(loc)
 
     gnt.grid(True) # Setting graph attribute
