5decbe4b61df59cb4a8b03fbfa19c3cdb29d6027
jleblond <jasmine.leblondc@gmail.com>
2020-12-12 23:57:14 -0500

    Modifs to disk scheduling output text

--- custom_helpers/print_helper.py
+++ custom_helpers/print_helper.py
@@ -3,10 +3,11 @@ def print_cylinders_diff(first_cylinder, second_cylinder, diff):
 
 def print_disk_scheduling_algorithm_info(algo_name, requests_queue, head_cylinder, is_moving_towards_0):
     print()
-    print("*** Computing " + str(algo_name) +"*** for " + str(requests_queue))
-    print('head points to cylinder: ' + str(head_cylinder) )
+    print("*** Computing " + str(algo_name) +"*** ")
+    print("_ requests queue: " + str(requests_queue))
+    print('_ head points to cylinder: ' + str(head_cylinder) )
     if is_moving_towards_0 != None:
-        print('is_moving_towards_0: ' + str(is_moving_towards_0))
+        print('_ is_moving_towards_0: ' + str(is_moving_towards_0))
 
 def print_disk_scheduling_algorithm_sum(sum):
     print('Total head movements: ' + str(sum))
